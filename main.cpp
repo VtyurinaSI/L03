4eef3d71d4acc1b48b4ec45011a711210d4d59d3
VtyurinaSI <svetlana0vtyurina@gmail.com>
2019-05-20 23:29:01 +0300

    max  и min функция

--- main.cpp
+++ main.cpp
@@ -11,7 +11,19 @@ input_numbers(size_t count) {
     }
     return result;
 }
-
+void
+find_minmax(vector<double> numbers, double& min, double& max) {
+    min = numbers[0];
+    max = numbers[0];
+    for (double number : numbers) {
+        if (number < min) {
+            min = number;
+        }
+        if (number > max) {
+            max = number;
+        }
+    }
+}
 int
 main() {
 
@@ -27,16 +39,7 @@ main() {
     cin >> bin_count;
 
 
-    double min = numbers[0];
-    double max = numbers[0];
-    for (double number : numbers) {
-        if (number < min) {
-            min = number;
-        }
-        if (number > max) {
-            max = number;
-        }
-    }
+
 
     vector<size_t> bins(bin_count);
     for (double number : numbers) {
